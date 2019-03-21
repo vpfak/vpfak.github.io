be14e3be5142675748d36f1cc09773f4189d9644
tfpak <48423431+tfpak@users.noreply.github.com>
2019-03-21 13:25:23 +0800

    Add files via upload

---
+++ gul/js/protect.js
@@ -0,0 +1,20 @@
+﻿document.onkeydown=function(){
+    var e = window.event||arguments[0];
+    if(e.keyCode==123){
+    	alert('唐方鹏博客主页提醒您!请尊重劳动成果！https://vpfak.github.io');
+            return false;
+    }else if((e.ctrlKey)&&(e.shiftKey)&&(e.keyCode==73)){
+    	alert('唐方鹏博客主页提醒您!请尊重劳动成果！https://vpfak.github.io');
+            return false;
+    }else if((e.ctrlKey)&&(e.keyCode==85)){
+            alert('唐方鹏博客主页提醒您!请尊重劳动成果！https://vpfak.github.io');
+            return false;
+    }else if((e.ctrlKey)&&(e.keyCode==83)){
+           alert('唐方鹏博客主页提醒您!请尊重劳动成果！https://vpfak.github.io');
+           return false;
+    }
+}
+document.oncontextmenu=function(){
+	alert('唐方鹏博客主页提醒您!请尊重劳动成果！https://vpfak.github.io');
+    return false;
+}
